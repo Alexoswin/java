ca3f70d677239c37bb342c32c9210ef3ccc58b30
oswin <oswinalex1@gmail.com>
2023-12-28 16:42:06 +0530

    loops

--- Loops.java
+++ Loops.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Loops {
 
 
     public  static void main (String[] args){
 
         System.out.println("----loops in java----");
-
+        
         Scanner sc  =new Scanner(System.in);
 
         System.out.println("enter a number  ");
@@ -14,9 +15,24 @@ public class Loops {
             System.out.println(x + " * " +i +" = "+ x*i);
         }
 
-        
+           int[] list =new int[20];
+     
+    for(int i = 0; i<list.length;i++){
+        Scanner ss = new Scanner(System.in);
+        System.out.println("enter the element for the " + i + " index position in the list " );
+        list[i] = ss.nextInt();
+    }
 
+    System.out.println("printing the elements of the list ");
+
+    for(int b =0 ;b <list.length;b++){
+
+        System.out.println(list[b]);
+    }
 
     }
+
+ 
+
     
 }
---
+++ Loops2.java
@@ -0,0 +1,27 @@
+public class Loops2 {
+    
+    public static void main (String[]args){
+        System.out.println("--- while and do while loops in java ---");
+
+                
+        
+        
+        System.out.println("----- while looop -----");
+
+        int i = 0;
+        while(i <10){
+            System.out.println(10*i);
+            i++;
+        }
+        
+        System.out.println("----- do while looop -----");
+
+        i = 1;
+
+        do{
+            System.out.println(20*i);
+            i ++;
+        }
+        while(i==0);
+    }
+}
