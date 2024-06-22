32de84cab1654cd600ee261d64cdeebb68c76739
Alexoswim <oswinalex1@gmail.com>
2024-06-22 17:14:06 +0530

    multi level inheritance

---
+++ new_OOP/access_modifiers.java
@@ -0,0 +1,13 @@
+
+class Account{
+
+    public String name ;
+}
+public class access_modifiers {
+ 
+    
+    public static void main (String [] args){
+
+        System.out.println("Access modifiers 4 tyes public, private ,protected and default ");
+    }
+}
--- new_OOP/c_inheritance.java
+++ new_OOP/c_inheritance.java
@@ -1,32 +1,66 @@
+/**
+ * c_inheritance
+ */
 
-class shapes{
+ class square{
 
-    String colour;  
+    int length;
+
+    
+    public void Area(int length){
+        
+        System.out.println(length * length);
+    }
+    
+    // constructor
+    square(int  length){
+
+        this.length = length;
+    }
+    
+ }
+
+class triangle extends square{
+
+    int height ;
+
+    
+    public void Area(int height , int length){
+        System.out.println(0.5 * length * height);
+    }
+    triangle(int length, int height){
+        super(length);
+        this.height = height;
+        
+    }
 }
 
-class triangle extends shapes{
 
-     int s1 ;
-     String type;
-
-     public void pinfo(){
-
-        System.out.println(this.colour + " " +  this.s1 + " " + this.type);
-     }
-
-
+class rectangle extends triangle {
+    public void Area(int length , int breath){
+    System.out.println(length* breath);
 }
+    rectangle(int length, int height){
 
+        super(length, height);
+    }
+}
 
 public class c_inheritance {
-    
 
-    public static void main(String[] args) {
-            triangle t1 = new triangle();
-            t1.colour = "blue";
-            t1.type = "right";
-            t1.s1 = 34;
-            t1.pinfo();
 
-    }
+     public static void main (String [] args){
+
+        System.out.println("multi level ingeitance ");
+
+
+        square one = new  square(6);
+        one.Area(one.length);
+
+        triangle t1 = new triangle(6, 2);
+        t1.Area(t1.length, t1.height);
+
+        rectangle r1 = new rectangle(13, 2);
+        r1.Area(r1.length, r1.height);
+     }
 }
